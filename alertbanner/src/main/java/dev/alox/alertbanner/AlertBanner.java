cccec26b893f6536bdeeed1f2df4f5893517e7bb
Alok J Kashyap <alokjkashyap@gmail.com>
2020-05-03 17:35:41 +0530

    Initial commit

---
+++ alertbanner/src/main/java/dev/alox/alertbanner/AlertBanner.java
@@ -0,0 +1,84 @@
+package dev.alox.alertbanner;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.ColorInt;
+import androidx.annotation.NonNull;
+import androidx.core.view.ViewCompat;
+
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.ContentViewCallback;
+
+public final class AlertBanner extends BaseTransientBottomBar<AlertBanner> {
+    /**
+     * Constructor for the transient bottom bar.
+     *
+     * @param parent              The parent for this transient bottom bar.
+     * @param content             The content view for this transient bottom bar.
+     * @param contentViewCallback The content view callback for this transient bottom bar.
+     */
+    protected AlertBanner(@NonNull ViewGroup parent, @NonNull View content, @NonNull com.google.android.material.snackbar.ContentViewCallback contentViewCallback) {
+        super(parent, content, contentViewCallback);
+    }
+
+    public static AlertBanner make(@NonNull ViewGroup parent, @Duration int duration){
+        parent.setRotation(180);
+        final LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        final View content = inflater.inflate(R.layout.alert_layout,parent,false);
+        final ContentViewCallbacks viewCallbacks = new ContentViewCallbacks(content);
+        final AlertBanner alertBanner = new AlertBanner(parent,content,viewCallbacks);
+
+        alertBanner.getView().setPadding(0,0,0,0);
+        alertBanner.setDuration(duration);
+        return alertBanner;
+    }
+
+    public AlertBanner setText(CharSequence text){
+        TextView textView = (TextView)getView().findViewById(R.id.snack_layout_text);
+        textView.setText(text);
+        return this;
+    }
+
+    public AlertBanner setBackgroundColor(@ColorInt int color){
+        LinearLayout mainlayout = (LinearLayout)getView().findViewById(R.id.snack_layout);
+        mainlayout.setBackgroundColor(color);
+        return this;
+    }
+
+
+
+    private static class ContentViewCallbacks implements BaseTransientBottomBar.ContentViewCallback {
+
+        private View content;
+
+        // view inflated from custom layout
+        public ContentViewCallbacks(View content){
+            this.content = content;
+        }
+
+        @Override
+        public void animateContentIn(int delay, int duration) {
+            // add custom *in animations for your views
+            // e.g. original snackbar uses alpha animation, from 0 to 1
+            ViewCompat.setScaleY(content, 0f);
+            ViewCompat.animate(content)
+                    .scaleY(1f).setDuration(duration)
+                    .setStartDelay(delay);
+        }
+
+        @Override
+        public void animateContentOut(int delay, int duration) {
+            // add custom *out animations for your views
+            // e.g. original snackbar uses alpha animation, from 1 to 0
+            ViewCompat.setScaleY(content, 1f);
+            ViewCompat.animate(content)
+                    .scaleY(0f)
+                    .setDuration(duration)
+                    .setStartDelay(delay);
+        }
+    }
+}
---
+++ app/src/main/java/dev/alox/alertbanner/MainActivity.java
@@ -0,0 +1,29 @@
+package dev.alox.alertbanner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertBanner alertBanner = AlertBanner.make((ViewGroup) findViewById(R.id.bannerView), Snackbar.LENGTH_LONG);
+                alertBanner.setText("No internet detected");
+                alertBanner.setBackgroundColor(getResources().getColor(R.color.red));
+                alertBanner.show();
+            }
+        });
+    }
+}
